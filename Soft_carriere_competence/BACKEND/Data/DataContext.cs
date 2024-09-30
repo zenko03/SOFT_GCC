2475c87f8a8a57519ed74ab8f93e30789c51d5f5
Toky Rabehaja <119029107+zenko03@users.noreply.github.com>
2024-09-30 12:03:58 +0300

    Deleting useless files

--- Soft_carriere_competence/BACKEND/Data/DataContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace soft_carriere_competence.Data
-{
-    public class DataContext: DbContext
-    {
-        public DataContext(DbContextOptions<DataContext> options):base(options) { }
-    }
-}
