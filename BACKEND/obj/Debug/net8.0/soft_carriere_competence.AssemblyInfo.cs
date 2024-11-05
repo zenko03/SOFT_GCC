dfcef7cbe2d723a62c975f8aeb03c426584247a7
Inssa chalman <118509826+Chalmaninssa19@users.noreply.github.com>
2024-11-05 07:41:16 +0100

    modification

--- BACKEND/obj/Debug/net8.0/soft_carriere_competence.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net8.0/soft_carriere_competence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soft_carriere_competence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1632e8dde7e35658633a7113dec58fd680074c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7750e7bcc539d3e485b7be1cb16445caecbdb8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("soft_carriere_competence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soft_carriere_competence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
