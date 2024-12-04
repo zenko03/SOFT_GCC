24a4b66f4e3a90af01b5407fa7fef6b0cf68ce7d
Toky Rabehaja <119029107+zenko03@users.noreply.github.com>
2024-12-04 14:10:45 +0300

    zavtara

--- BACKEND/obj/Debug/net8.0/soft_carriere_competence.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net8.0/soft_carriere_competence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soft_carriere_competence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7750e7bcc539d3e485b7be1cb16445caecbdb8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfcef7cbe2d723a62c975f8aeb03c426584247a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("soft_carriere_competence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soft_carriere_competence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
