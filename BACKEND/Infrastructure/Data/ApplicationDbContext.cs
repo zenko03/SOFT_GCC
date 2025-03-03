2bf27d0ab7c83ca9e86571673bda3767cdac633a
Inssa chalman <118509826+Chalmaninssa19@users.noreply.github.com>
2025-03-03 16:54:07 +0300

    y

--- BACKEND/Infrastructure/Data/ApplicationDbContext.cs
+++ BACKEND/Infrastructure/Data/ApplicationDbContext.cs
@@ -178,6 +178,7 @@ namespace soft_carriere_competence.Infrastructure.Data
 			modelBuilder.Entity<VDepartmentEffective>().HasNoKey();
 		}
 	}
+<<<<<<< Updated upstream
 
             //    // Configuration de la relation EvaluationInterviews - InterviewParticipants
             //    modelBuilder.Entity<InterviewParticipants>()
@@ -191,6 +192,8 @@ namespace soft_carriere_competence.Infrastructure.Data
             //        .WithMany()
             //        .HasForeignKey(p => p.UserId)
             //        .OnDelete(DeleteBehavior.Restrict); // Pas de suppression d'utilisateur si le participant est supprimé
+=======
+>>>>>>> Stashed changes
 }
 
 
